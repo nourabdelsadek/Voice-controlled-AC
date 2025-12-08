71a4c7c24809fa1b7b9d51b7aa9164ec1ad4faa1
Noureldien Abdelsadek <you@example.com>
2025-12-09 00:34:09 +0200

    DC control libraries

---
+++ DC_Motor.h
@@ -0,0 +1,18 @@
+#ifndef __DC_MOTOR
+#define __DC_MOTOR
+
+typedef enum {
+    DIRECTION_CW = 0,
+    DIRECTION_CCW
+} tenuDirection;
+
+void DC_Init();
+void DC_Start(unsigned char MotorID, tenuDirection Direction, unsigned char Speed);
+void DC_Stop(unsigned char MotorID);
+/*
+void fan(int speed);
+void hood(int speed);
+void stop();
+*/
+
+#endif
